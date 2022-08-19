d185d76824d7ee335d4fb2a91a9b89a47538baaa
PromotezCitezen <gkstkdgus821@gmail.com>
2022-08-19 16:48:50 +0900

    [heroku] .7 flask 없이 request 되는지 확인

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def crawling():
     
     None
  
-crawling()
 
-# if __name__ == '__main__':
-#     app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    # app.run(host="0.0.0.0", port=5000, debug=True)
+    crawling()

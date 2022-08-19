2cbbfd4a696ee2609bda5fe132cbb32a14bb5431
PromotezCitezen <gkstkdgus821@gmail.com>
2022-08-19 17:17:57 +0900

    [heroku] 1.4 flask 이용, 페이지 뜨는지 확인

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import src
 import json
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -40,5 +41,6 @@ def main():
  
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
     # crawling()

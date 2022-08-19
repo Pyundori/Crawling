82f6eb9ae7cc64fb32a0817453c8d7152ce36f1e
PromotezCitezen <gkstkdgus821@gmail.com>
2022-08-19 15:07:20 +0900

    [migration] window 개발환경 -> linux 개발환경. 크롤링을 총괄하는 메인함수

---
+++ main.py
@@ -0,0 +1,31 @@
+import src
+
+PAGE_LIST = {
+    'seven_eleven': 'https://www.7-eleven.co.kr/product/listMoreAjax.asp',
+    'gs25': 'http://gs25.gsretail.com/gscvs/ko/products/event-goods-search',
+    'emart24': 'https://www.emart24.co.kr/product/eventProduct.asp',
+    'cu': 'https://cu.bgfretail.com/event/plusAjax.do',
+}
+        
+# main function
+def main():
+    seven_eleven = src.POSTRequestAPI_SevenEleven(PAGE_LIST["seven_eleven"]) # 완료
+    # with open("seven_eleven.json", "w", encoding='utf-8') as f:
+    #     f.write(json.dumps(seven_eleven, ensure_ascii=False))
+
+    gs25 = src.GETRequestAPI_Gs25(PAGE_LIST["gs25"]) # 완료
+    # with open("gs25.json", "w", encoding='utf-8') as f:
+    #     f.write(json.dumps(gs25, ensure_ascii=False))
+
+    emart24 = src.GETRequestAPI_Emart24(PAGE_LIST['emart24']) # 완료
+    # with open("emart24.json", "w", encoding='utf-8') as f:
+    #     f.write(json.dumps(emart24, ensure_ascii=False))
+
+    cu = src.POSTRequestAPI_Cu(PAGE_LIST['cu']) # 완료
+    # with open("cu.json", "w", encoding='utf-8') as f:
+    #     f.write(json.dumps(cu, ensure_ascii=False))
+
+    None
+ 
+if __name__ == '__main__':
+    main()

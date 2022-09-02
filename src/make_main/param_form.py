e77a6b44e586c9662a41010319776a2d1070f75a
PromotezCitezen <gkstkdgus821@gmail.com>
2022-09-02 09:12:07 +0000

    [Mainpage] form 2개를 메인 파일에서 분리

---
+++ src/make_main/param_form.py
@@ -0,0 +1,118 @@
+def params_query(link):
+    form = f"""<form action='{link}' method='GET'>
+        <p>venders=
+        <label><input type='checkbox' name='venders' value='cu'>cu</label>
+        <label><input type='checkbox' name='venders' value='gs25'>gs25</label>
+        <label><input type='checkbox' name='venders' value='seven_eleven'>seven_eleven</label>
+        <label><input type='checkbox' name='venders' value='emart24'>emart24</label>
+        <label><input type='checkbox' name='venders' value='ministop'>ministop</label>
+          | type: str</p>
+        <p>dtypes= 
+        <label><input type='checkbox' name='dtypes' value='1N1'>1N1</label>
+        <label><input type='checkbox' name='dtypes' value='2N1'>2N1</label>
+        <label><input type='checkbox' name='dtypes' value='3N1'>3N1</label>
+        <label><input type='checkbox' name='dtypes' value='SALE'>SALE</label>
+        <label><input type='checkbox' name='dtypes' value='GIFT'>GIFT</label>
+          | type: str</p>
+        <p>products= <input type='text' name='products'>  | type: str</p>
+        <p>page= <input type='text' name='page'>  | type: int, 1 이상의 값</p>
+        <p><input type='submit' value='제출'></p>
+        </form>"""
+    return form
+
+def params_dup(link):
+    form = f"""
+        <form action='{link}' method='GET'>
+        <p>column=
+        <label><input type='radio' name='column' value='id' checked='checked'>id</label>
+        <label><input type='radio' name='column' value='name'>name</label>
+        <label><input type='radio' name='column' value='email'>email</label>
+          | type: str</p>
+        <p>data= <input type='text' name='data'>  | type: str</p>
+        <p><input type='submit' value='제출'></p>
+        </form>"""
+
+    return form
+
+def params_signup(link):
+    form = f"""<form action='{link}' method='POST'>
+        <p>id= <input type='text' name='id'>  | type: str, max len: 20</p>
+        <p>pw= <input type='text' name='pw'></p>
+        <p>name= <input type='text' name='name'>  | type: str, max len: 20(kor: 10)</p>
+        <p>email= <input type='text' name='email'>  | type: str, max len: 50</p>
+        <p><input type='submit' value='제출'></p>
+        </form>"""
+
+    return form
+
+def params_signin(link):
+    form = f"""<form action='{link}' method='POST'>
+        <p>id= <input type='text' name='id'>  | type: str</p>
+        <p>pw= <input type='text' name='pw'>  | type: str</p>
+        <p>token= <input type='text' name='token'>  | type: str</p>
+        <p><input type='submit' value='제출'></p>
+        </form>"""
+
+    return form
+    
+def params_user_data(link):
+    form = f"""<form action='{link}' method='POST'>
+        <p>token= <input type='text' name='token'>  | type: str</p>
+        <p><input type='submit' value='제출'></p>
+        </form>"""
+
+    return form
+
+def params_user_modify(link):
+    form = f"""<form action='{link}' method='POST'>
+        <p>token= <input type='text' name='token'>  | type: str</p>
+        <p>col= <input type='text' name='col'>  | type: str, value: pw, email</p>
+        <p>data= <input type='text' name='data'> | type: str</p>
+        <p><input type='submit' value='제출'></p>
+        </form>"""
+
+    return form
+
+def params_product_like(link):
+    form = f"""<form action='{link}' method='POST'>
+        <p>vender= <input type='text' name='vender'>  | type: str</p>
+        <p>pName= <input type='text' name='pName'>  | type: str</p>
+        <p>flag= <input type='text' name='flag'>  | type: str, value: like or else..</p>
+        <p><input type='submit' value='제출'></p>
+        </form>"""
+
+    return form
+
+def params_product_like_ranking(link):
+    form = f"""<form action='{link}' method='GET'>
+        <p><input type='submit' value='제출'></p>
+        </form>"""
+
+    return form
+
+def params(flag, args):
+    ret = []
+    ret.append('<button type="button" class="collapsible">params</button>')
+    ret.append('<div class="content">')
+
+    if flag == "dup":
+        data = params_dup(args['check_dup'])
+    elif flag == "query":
+        data = params_query(args['from_db_select_query'])
+    elif flag == "signup":
+        data = params_signup(args['sign_up'])
+    elif flag == "signin":
+        data = params_signin(args['sign_in'])
+    elif flag == "userdata":
+        data = params_user_data(args['get_user'])
+    elif flag == "usermodify":
+        data = params_user_modify(args['user_modify'])
+    elif flag == "productlike":
+        data = params_product_like(args['product_like'])
+    elif flag == "likeranking":
+        data = params_product_like_ranking(args['like_ranking'])
+
+    ret.append(data)
+    ret.append("</div>")
+
+    return "".join(ret)
---
+++ src/make_main/return_value.py
@@ -0,0 +1,114 @@
+
+SPACE = "&nbsp;&nbsp;&nbsp;&nbsp;"
+
+def return_value_query():
+    data = [
+        '{',
+        f'{SPACE}response_code: int - 201: searched data exists, 202: searched data not exists',
+        f'{SPACE}data_cnt: int - 쿼리의 전체 데이터 개수',
+        f'{SPACE}data: [',
+        f'{SPACE}{SPACE}%s' % "{",
+        f'{SPACE}{SPACE}{SPACE}vender: str,',
+        f'{SPACE}{SPACE}{SPACE}dType : str,',
+        f'{SPACE}{SPACE}{SPACE}pName	: str,',
+        f'{SPACE}{SPACE}{SPACE}pPrice: int,',
+        f'{SPACE}{SPACE}{SPACE}pImg  : str,',
+        f"{SPACE}{SPACE}%s, ..." % "}",
+        f"{SPACE}]",
+        "}",
+    ]
+    return data
+
+def return_value_dup():
+    data = [
+        '{',
+        f'{SPACE}res_code: int - 201: can craete this name/id/email, 202: cannot craete this name/id/email',
+        f'{SPACE}{SPACE}{SPACE}{SPACE}{SPACE}{SPACE}400: cannot read properties, cause data value is \"\" or column value is \"\"',
+        "}",
+    ]
+    return data
+
+def return_value_signup():
+    data = [
+        '{',
+        f'{SPACE}res_code: int - 201: append user to DB, 500: duplicate id/name/email in DB',
+        "}",
+    ]
+    return data
+
+def return_value_signin():
+    data = [
+        '{',
+        f'{SPACE}res_code: int - 500: No user with id in DB, 501: PW isn\'t correct',
+        f'{SPACE}{SPACE}{SPACE}{SPACE}{SPACE}502: invalid token',
+        f'{SPACE}{SPACE}{SPACE}{SPACE}{SPACE}201: login success, 202: valid token',
+        f'{SPACE}token: "" - return when login failed, jwt_tolen - return when login success',
+        "}",
+    ]
+    return data
+
+def return_value_user_data():
+    data = [
+        '{',
+        f'{SPACE}res_code: int - 201: token valid, 400: token invalid',
+        f'{SPACE}id: str',
+        f'{SPACE}name: str',
+        f'{SPACE}email: str',
+        "}",
+    ]
+    return data
+
+def return_value_user_modify():
+    data = [
+        '{',
+        f'{SPACE}res_code: int - 400: column value is not correct, 500: invalid token',
+        f'{SPACE}{SPACE}{SPACE}{SPACE}{SPACE}201: modify succeed',
+        f'{SPACE}data: token - if changed => changed token, else => origin token',
+        "}",
+    ]
+    return data
+
+def return_value_product_like():
+    data = [
+        '{',
+        f'{SPACE}res_code: int - 201: change successful, 400: change failed',
+        "}",
+    ]
+    return data
+
+def return_value_product_like_ranking():
+    data = [
+        '{',
+        f'{SPACE}&lt;vender&gt;&&lt;product_name&gt;: int , key를 &로 분해하여 사용',
+        '...',
+        "}",
+    ]
+    return data
+    
+def return_value(flag):
+    ret = []
+    ret.append('<button type="button" class="collapsible">return value</button>')
+    ret.append('<div class="content">')
+
+    if flag == "dup":
+        data = return_value_dup()
+    elif flag == "query":
+        data = return_value_query()
+    elif flag == "signup":
+        data = return_value_signup()
+    elif flag == "signin":
+        data = return_value_signin()
+    elif flag == "userdata":
+        data = return_value_user_data()
+    elif flag == "usermodify":
+        data = return_value_user_modify()
+    elif flag == "productlike":
+        data = return_value_product_like()
+    elif flag == "likeranking":
+        data = return_value_product_like_ranking()
+
+    for dat in data:
+        ret.append(f"<p>{dat}</p>")
+    ret.append("</div>")
+
+    return "".join(ret)
